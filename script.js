e48c99365e1e8ec6a8b3dfd772a238cf2f03d376
vikashkrr408-commits <vikashkrr408@gmail.com>
2026-03-19 02:40:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,50 @@ jobs[i].style.display="none"
 }
 
 }o TNas Job Search")
+let data = [
+{title:"Indian Navy Recruitment 2026", type:"jobs"},
+{title:"Railway Group D Result", type:"results"},
+{title:"SSC GD Admit Card", type:"admit"},
+{title:"UP Police Answer Key", type:"answer"},
+{title:"PM Awas Yojana", type:"yojana"},
+{title:"Scholarship 2026", type:"scholarship"},
+{title:"Upcoming SSC Jobs", type:"upcoming"}
+];
+
+function loadData(){
+
+data.forEach(item=>{
+
+let div=document.createElement("div")
+div.innerHTML="<p>"+item.title+"</p>"
+
+document.getElementById(item.type).appendChild(div)
+
+})
+
+}
+
+loadData()
+
+function searchData(){
+
+let input=document.getElementById("search").value.toLowerCase()
+
+let sections=document.querySelectorAll(".section")
+
+sections.forEach(sec=>sec.innerHTML="")
+
+data.forEach(item=>{
+
+if(item.title.toLowerCase().includes(input)){
+
+let div=document.createElement("div")
+div.innerHTML="<p>"+item.title+"</p>"
+
+document.getElementById(item.type).appendChild(div)
+
+}
+
+})
+
+}
